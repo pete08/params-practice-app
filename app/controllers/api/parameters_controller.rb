9e582c8eae125f33cee67d02f378ef7ef052967a
PeterH <pete08@gmail.com>
2020-07-02 16:15:19 -0500

    Add query parameters to route. sends parameter string back in all caps

--- app/controllers/api/parameters_controller.rb
+++ app/controllers/api/parameters_controller.rb
@@ -1,2 +1,9 @@
 class Api::ParametersController < ApplicationController
+
+def capstring
+  @word = params["string"].upcase
+  render "display.json.jb"
+end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,10 @@ Rails.application.routes.draw do
   # namespace :api do
   #   get "/photos" => "photos#index"
   # end
+
+  namespace :api do
+    get "/caps" => "parameters#capstring"
+  end
+
+
 end
